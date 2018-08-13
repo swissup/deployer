e0aca8f9cfd030498ab2c17b07ee8c2b36ece21a
Alexander Kras'ko <0m3r.mail@gmail.com>
2018-08-13 13:46:41 +0300

    Move some defaults to deploy.php

--- deploy.php
+++ deploy.php
@@ -8,10 +8,15 @@ require_once __DIR__ . '/recipe/common.php';
 require_once __DIR__ . '/recipe/magento1/backup.php';
 require_once __DIR__ . '/recipe/magento2/backup.php';
 
-set('keep_releases', 30);
+set('keep_releases', 5);
 set('shared_dirs', []);
 set('shared_files', []);
-// set('ssh_type', 'native');
+set('use_relative_symlink', false);
+set('use_atomic_symlink', false);
+set('ssh_type', 'native');
 set('default_stage', 'production');
+set('mysql_db', false);
+set('writable_use_sudo', true);
+set('add_sample_data', false);
 
 inventory('hosts.yml');

8e786dec7713050c383e29365a1f7b8ccf63a7ac
Alexander Kras'ko <0m3r.mail@gmail.com>
2018-08-15 09:24:53 +0300

    Disable app:config:[dump|import] tasks

--- deploy.php
+++ deploy.php
@@ -10,18 +10,19 @@ require_once __DIR__ . '/recipe/common.php';
 require_once __DIR__ . '/recipe/magento2.php';
 
 set('keep_releases', 5);
+set('default_stage', 'production');
 set('use_relative_symlink', false);
 set('use_atomic_symlink', false);
+set('writable_use_sudo', true);
+
 set('port', 22);
 set('ssh_type', 'native');
 set('forwardAgent', true);
 set('multiplexing', true);
-set('default_stage', 'production');
-set('mysql_db', false);
-set('writable_use_sudo', true);
+
 set('add_sample_data', false);
 set('mysql_host', '127.0.0.1');
-
+set('mysql_db', false);
 // set('packages', false);
 
 inventory('hosts.yml');

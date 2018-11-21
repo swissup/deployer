a513932f204fee65e48ea9a3b9f25891f2e111a8
Alexander Kras'ko <0m3r.mail@gmail.com>
2018-11-21 11:57:55 +0200

    Add/ Return magento2:deploy:modules:enable

---
+++ recipe/magento2/deploy/modules.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Deployer;
+
+require_once CUSTOM_RECIPE_DIR . '/bin/magento.php';
+
+task('magento2:deploy:modules:enable', function () {
+    $modules = get('option_modules');
+    // print_r($packages);
+    if (empty($modules)) {
+        $modules = '--all';
+    } else {
+        $modules = implode(' ', $modules);
+    }
+    // $status = run("cd {{release_path}} && {{bin/magento}} module:status");
+    // $rm = 'None';
+    // $status = str_replace($rm, '', $status);
+    // $delimiter ='List of disabled modules:';
+    // list($enable, $disable) = explode($delimiter, $status);
+    // $rm = 'List of enabled modules:';
+    // // $enable = str_replace($rm, '', $enable);
+    // $modules = explode("\n", $disable);
+    // $modules = array_filter($modules);
+
+    $options = '';//' --clear-static-content --force ';
+    run(
+        "cd {{release_path}} "
+        . "&& {{bin/magento}} module:enable $options $modules"
+        . "&& {{bin/magento}} setup:upgrade"
+    );
+})->setPrivate();

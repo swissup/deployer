9fb19814f0f1f844917289a72f615b006c875564
Alexander Kras'ko <0m3r.mail@gmail.com>
2019-03-19 10:30:01 +0200

    Upgraded magento2:deploy:permissions task

--- recipe/magento2/deploy/permissions.php
+++ recipe/magento2/deploy/permissions.php
@@ -19,16 +19,19 @@ task('magento2:deploy:permissions', function () {
 
     $commands = array(
         "{{bin/sudo}} chown -R $user:$group . ",
-        "{{bin/sudo}} find . -type d -exec chmod 770 {} \; ",
-        "{{bin/sudo}} find . -type f -exec chmod 660 {} \; ",
-        //"{{bin/sudo}} find . -type l -exec chmod 660 {} \; ",
-         "{{bin/sudo}} chmod u+x bin/magento",
+        "{{bin/sudo}} find . -type d -exec chmod 775 {} \; ",
+        "{{bin/sudo}} find . -type f -exec chmod 664 {} \; ",
+        "{{bin/sudo}} find . -type l -exec chmod 664 {} \; ",
+        "{{bin/sudo}} chmod u+x bin/magento",
+        "{{bin/sudo}} find var generated vendor pub/static pub/media app/etc -type f -exec chmod u+w {} +",
+        "{{bin/sudo}} find var generated vendor pub/static pub/media app/etc -type d -exec chmod u+w {} +",
+        // "{{bin/sudo}} find var generated vendor pub/static pub/media app/etc -type l -exec chmod u+w {} +"
         //"{{bin/sudo}} chmod g+x {{bin/magento}} ",
         //"{{bin/sudo}} chmod -R g+w {app/etc,pub,var,vendor}",
-        "{{bin/sudo}} chmod -R g+w app/etc ",
-        "{{bin/sudo}} chmod -R g+w pub",
-        "{{bin/sudo}} chmod -R g+w var",
-        "{{bin/sudo}} chmod -R g+w generated"
+        // "{{bin/sudo}} chmod -R g+w app/etc ",
+        // "{{bin/sudo}} chmod -R g+w pub",
+        // "{{bin/sudo}} chmod -R g+w var",
+        // "{{bin/sudo}} chmod -R g+w generated"
     );
     foreach ($commands as $command) {
         run("cd {{release_path}} && " . $command);
